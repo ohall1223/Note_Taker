f82b01725d3fdadd5ba98303ddd2dd08b0ceaf96
Olivia Hall <ohall1223@gmail.com>
2021-12-08 15:12:32 -0700

    Working on routes

--- Develop/server.js
+++ Develop/server.js
@@ -5,41 +5,43 @@ const path = require("path");
 const database = require("./db/db")
 
 // express app
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+let notes = []
 
 // link assets
 app.use(express.static("public"));
 
-// format to JSON
+// middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-fs.readFile(database, "utf8", (err, data) => {
-    if(err) throw err
-
-    var notes = JSON.parse(data)
   
-    // GET notes route
-    app.get("/api/notes", (req, res) => {
-        res.json(notes);
-    })
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "index.html"));
+// })
 
-    // post to notes route
-    app.post("/api/notes", (req, res) => {
-        var filePath = path.join(_dirname, "/db/db.json");
-        var newNote= req.body;
-        
-        notes.push(newNote);
+// app.get("/api/notes", (req, res) => {
+//     notes = fs.readFileSync("database")
+//     res.json(notes)
+// })
 
-        console.log("added new note: " + newNote.title)
-        return;
-    })
+// app.post("/api/notes", (req, res) => {
+//     notes = fs.read(database)
+//     console.log(notes)
+//     notes = JSON.parse(notes);
+//     req.body.id = notes.length;
+//     notes.push(req.body);
+//     notes = JSON.stringify(notes)
+//     fs.writeFile(database, notes, (err) => {
+//         if(err)
+//         throw err
+//     })
+//     res.json(JSON.parse(notes))
+// })
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
-
-    app.listen(PORT, () => {
-        console.log("App listening on port " + PORT)
-    })
 
 
---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+
+// route handlers
+// route to get all notes
+router.get("/notes", (req, res) => {
+    // interact with the database table of notes
+})
+
+// route to post a new note
+
+// route to get a specific note
+
+// route to delete a specific note
+
+// route to updata a note
