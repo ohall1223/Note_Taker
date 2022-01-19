7ce3728ea17b93ea9ccecc982fbed438c7fb736f
Olivia Hall <ohall1223@gmail.com>
2022-01-19 14:52:50 -0700

    routes added back

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router()
+const { request } = require("http");
+const store = require("../db/store")
+
+
+// route handlers
+// route to get all notes
+router.get("/notes", (req, res) => {
+    store.getNotes()
+    .then((notes) => 
+        res.json(notes)
+    )
+    .catch(err => res.status(500).json(err))
+});
+
+// route to post a new note
+router.post("/notes", (req, res) => {
+    store.addNote(req.body)
+    // data will be stored that came from the front end on req.body
+    .then((note) => 
+        res.json(note)
+    )
+    .catch(err => res.status(500).json(err))
+})
+
+// route to delete a specific note
+router.delete('/notes/:id', (req, res) => {
+    store.deleteNote(req.params.id)
+    .then(() => res.json({ok: true}))
+    .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
